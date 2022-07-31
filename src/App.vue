0a8b23235985bf7b74fa26401b2f79d37bd8eb69
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2022-07-31 12:23:44 +0700

    Lint code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div class='max-w-4xl mx-auto p-8'>
     <header>
-      <h1 class='text-2xl mb-4'>Viet Nam personal ID decoder</h1>
+      <h1 class='text-2xl mb-4'>
+        Viet Nam personal ID decoder
+      </h1>
     </header>
     <main class='mx-auto'>
       <form @submit.prevent='onSubmit'>
         <div class='flex items-center space-x-4'>
           <label class='block'>Personal ID (12 digit)</label>
-          <input v-model='personalId' type='text' minlength='12' autocomplete='off'
-          class='grow focus:ring-indigo-500 focus:border-indigo-500 shadow-sm sm:text-sm border-gray-300 rounded-md'>
+          <input
+            v-model='personalId'
+            type='text'
+            minlength='12'
+            autocomplete='off'
+            class='grow focus:ring-indigo-500 focus:border-indigo-500 shadow-sm sm:text-sm border-gray-300 rounded-md'
+          >
         </div>
       </form>
-      <ResultDisplay v-if='personalInfo' :result='personalInfo' />
+      <ResultDisplay
+        v-if='personalInfo'
+        :result='personalInfo'
+      />
     </main>
   </div>
 </template>
--- src/components/ResultDisplay.vue
+++ src/components/ResultDisplay.vue
@@ -1,4 +1,6 @@
-<template></template>
+<template>
+  <div />
+</template>
 
 <script setup lang='ts'>
 import { PersonalInfo } from '@/models'
