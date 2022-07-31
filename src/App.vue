5961ab161a99004f2c65901667e333e912d8c914
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2022-07-31 11:48:32 +0700

    Simple UI

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
 <template>
-  <main class='mx-auto p-8'>
+  <div class='max-w-4xl mx-auto p-8'>
     <header>
       <h1 class='text-2xl mb-4'>Viet Nam personal ID decoder</h1>
     </header>
-  </main>
+    <main class='mx-auto'>
+      <form @submit.prevent='onSubmit'>
+        <div class='flex items-center space-x-4'>
+          <label class='block'>Personal ID (12 digit)</label>
+          <input v-model='personalId' type='text' minlength='12' autocomplete='off'
+          class='grow focus:ring-indigo-500 focus:border-indigo-500 shadow-sm sm:text-sm border-gray-300 rounded-md'>
+        </div>
+      </form>
+      <ResultDisplay v-if='personalInfo' :result='personalInfo' />
+    </main>
+  </div>
 </template>
+
+<script setup lang='ts'>
+import { ref } from 'vue'
+
+import { PersonalInfo } from '@/models'
+import ResultDisplay from '@/components/ResultDisplay.vue'
+
+const personalId = ref('')
+const personalInfo = ref<PersonalInfo | null>(null)
+
+function onSubmit() {
+  if (personalId.value.length < 12) {
+    return
+  }
+}
+
+</script>
---
+++ src/components/ResultDisplay.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script setup lang='ts'>
+import { PersonalInfo } from '@/models'
+
+interface Props {
+  result: PersonalInfo
+}
+
+defineProps<Props>()
+</script>
