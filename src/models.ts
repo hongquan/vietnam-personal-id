78b5d7e32189463a7cf8b8c99d0ff92975f7b563
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2022-08-01 22:04:34 +0700

    Delete unused code

--- src/models.ts
+++ src/models.ts
@@ -1,4 +1,4 @@
-import { array, assign, defaulted, Infer, number, object, string } from 'superstruct'
+import { Infer, number, object, string } from 'superstruct'
 
 export enum Gender {
   Male,
@@ -12,23 +12,10 @@ export interface PersonalInfo {
   random_number: string,
 }
 
-export const BaseSchema = object({
+export const ProvinceSchema = object({
   code: number(),
   name: string(),
 })
 
-export const WardSchema = BaseSchema
-
-export const DistrictShema = assign(BaseSchema, object({
-  wards: defaulted(array(WardSchema), []),
-}))
-
-export const ProvinceSchema = assign(BaseSchema, object({
-  districts: defaulted(array(DistrictShema), []),
-}))
-
-export type Base = Infer<typeof BaseSchema>
-export type Ward = Infer<typeof WardSchema>
-export type District = Infer<typeof DistrictShema>
 export type Province = Infer<typeof ProvinceSchema>
 
