def5f5a630854bec071d9116729f5f2491584985
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2022-08-03 18:11:22 +0700

    Delete obsolete comment

--- src/utils.ts
+++ src/utils.ts
@@ -11,7 +11,6 @@ interface Country {
 export async function extractBirthplace(idNumber: string) {
   const code = idNumber.slice(0, 3)
   if (code[0] !== '0') {
-    // TODO: Handle code for foreign countries.
     // Ref: https://thuvienphapluat.vn/van-ban/Quyen-dan-su/Thong-tu-07-2016-TT-BCA-quy-dinh-chi-tiet-luat-can-cuoc-cong-dan-nghi-dinh-137-2015-ND-CP-304996.aspx
     const data = (countries as Record<string, Country>)[code]
     if (!data) {
